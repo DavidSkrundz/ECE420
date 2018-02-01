b71242a5117ec98fad3886b824141c27d95463f7
David Skrundz <david@skrundz.ca>
2018-02-01 11:10:00 -0700

    start lab2 with terrible code

---
+++ lab2/src/cast.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define INT2VOIDP(i) (void*)(uintptr_t)(i)
+#define VOIDP2INT(p) (int)(uintptr_t)(p)
---
+++ lab2/src/server.c
@@ -0,0 +1,50 @@
+#include "cast.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+//#include<stdlib.h>
+//#include<sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *ServerEcho(void *args) {
+	int clientFileDescriptor = VOIDP2INT(args);
+	char str[20];
+	
+	read(clientFileDescriptor, str, 20);
+	printf("nreading from client:%s", str);
+	write(clientFileDescriptor, str, 20);
+	printf("nechoing back to client");
+	close(clientFileDescriptor);
+	return NULL;
+}
+
+
+int main() {
+	struct sockaddr_in sock_var;
+	int serverFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+	int clientFileDescriptor;
+	pthread_t t[20];
+	
+	sock_var.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sock_var.sin_port = 3000;
+	sock_var.sin_family = AF_INET;
+	if(bind(serverFileDescriptor, (struct sockaddr*)&sock_var, sizeof(sock_var)) >= 0) {
+		printf("nsocket has been created");
+		listen(serverFileDescriptor, 2000);
+		while(true) {
+			for(int i = 0; i < 20; ++i) {
+				clientFileDescriptor = accept(serverFileDescriptor, NULL, NULL);
+				printf("nConnected to client %dn", clientFileDescriptor);
+				pthread_create(&t[i], NULL, ServerEcho, INT2VOIDP(clientFileDescriptor));
+			}
+		}
+		close(serverFileDescriptor);
+	} else{
+		printf("nsocket creation failed");
+	}
+	return 0;
+}
